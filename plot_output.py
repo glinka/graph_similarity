2c7f23e17e25a1974674cbd6be51d3edc4702ce7
tchaikovsky <holiday@princeton.edu>
2014-09-22 14:09:49 -0400

    more plotting routines

--- plot_output.py
+++ plot_output.py
@@ -49,7 +49,7 @@ def plot_chunglu_deg_dists():
                 graphs.append(graph)
             plot_avg_degree_dist(graphs, params)
 
-def plot_chunglu_coloring(eigvals, eigvects, graph_params, ax=None, nplots=None):
+def plot_chunglu_coloring(eigvals, eigvects, graph_params, output_filename=None, ax=None, nplots=None):
     if ax is None:
         fig = plt.figure()
         ax = fig.add_subplot(111)
@@ -65,6 +65,10 @@ def plot_chunglu_coloring(eigvals, eigvects, graph_params, ax=None, nplots=None)
     # also, must take the "bottom"
     # vectors as the eigenvalues were sorted
     # low to high
+    if output_filename is None:
+        output_filename = ""
+    else:
+        output_filename = output_filename + "_"
     ax.set_xlim((np.min(pvals), np.max(pvals)))
     ax.set_ylim((np.min(rvals), np.max(rvals)))
     ax.set_xlabel('p')
@@ -74,9 +78,9 @@ def plot_chunglu_coloring(eigvals, eigvects, graph_params, ax=None, nplots=None)
     for i in range(2, nplots+2):
         ax.scatter(pvals, rvals, c=eigvects[-i], s=50, lw=0, alpha=0.7)
         ax.set_title("Chung-Lu ensemble coloring: eigenvector " + str(i-1))
-        plt.savefig("./figs/chunglu_coloring/eigvect_" + str(i-1) + ".png")
+        plt.savefig("./figs/chunglu_coloring/" + output_filename + "eigvect_" + str(i-1) + ".png")
 
-def plot_erdosrenyi_coloring(eigvals, eigvects, graph_params, ax=None, nplots=4):
+def plot_erdosrenyi_coloring(eigvals, eigvects, graph_params, output_filename=None, ax=None, nplots=4):
     if ax is None:
         fig = plt.figure()
         ax = fig.add_subplot(111)
@@ -91,6 +95,10 @@ def plot_erdosrenyi_coloring(eigvals, eigvects, graph_params, ax=None, nplots=4)
     # also, must take the "bottom"
     # vectors as the eigenvalues were sorted
     # low to high
+    if output_filename is None:
+        output_filename = ""
+    else:
+        output_filename = output_filename + "_"
     ax.set_xlim((np.min(pvals), np.max(pvals)))
     ax.set_xlabel('p')
     ax.set_yticks([])
@@ -99,9 +107,9 @@ def plot_erdosrenyi_coloring(eigvals, eigvects, graph_params, ax=None, nplots=4)
     for i in range(2, nplots+2):
         ax.scatter(pvals, np.zeros(n), c=eigvects[-i], s=50, lw=0, alpha=0.7)
         ax.set_title("Erdos-Renyi ensemble coloring: eigenvector " + str(i-1))
-        plt.savefig("./figs/erdosrenyi_coloring/eigvect_ " + str(i-1) + ".png")
+        plt.savefig("./figs/erdosrenyi_coloring/" + output_filename + "eigvect_ " + str(i-1) + ".png")
 
-def plot_erdosrenyi_embedding(eigvals, eigvects, graph_params, ax=None, maxindex=5, t=0):
+def plot_erdosrenyi_embedding(eigvals, eigvects, graph_params, output_filename=None, ax=None, maxindex=5, t=0):
     if ax is None:
         fig = plt.figure()
         ax = fig.add_subplot(111)
@@ -119,6 +127,10 @@ def plot_erdosrenyi_embedding(eigvals, eigvects, graph_params, ax=None, maxindex
     eigvects_to_plot = np.array([eigvects[-i,:] for i in range(2, maxindex+1)])
     eigvals_to_plot = np.array([eigvals[-i] for i in range(2, maxindex+1)])
     nvects = maxindex - 1
+    if output_filename is None:
+        output_filename = ""
+    else:
+        output_filename = output_filename + "_"
     for i in range(nvects):
         for j in range(i+1, nvects):
             xvals = np.power(eigvals_to_plot[i], t)*eigvects_to_plot[i,:]
@@ -129,7 +141,7 @@ def plot_erdosrenyi_embedding(eigvals, eigvects, graph_params, ax=None, maxindex
             ax.set_xlabel('Eigvect ' + str(i+1))
             ax.set_ylabel('Eigvect ' + str(j+1))
             ax.set_title('Erdos-Renyi embedding')
-            plt.savefig("./figs/erdosrenyi_embedding/eigvects_ " + str(i+1) + str(j+1) + ".png")
+            plt.savefig("./figs/erdosrenyi_embedding/" + output_filename + "eigvects_ " + str(i+1) + str(j+1) + ".png")
 
 
 if __name__=="__main__":
@@ -141,6 +153,7 @@ if __name__=="__main__":
     parser.add_argument('--chunglu-coloring', '-cc', action='store_true', default=False)
     parser.add_argument('--erdosrenyi-coloring', '-ec', action='store_true', default=False)
     parser.add_argument('--erdosrenyi-embedding', '-ee', action='store_true', default=False)
+    parser.add_argument('--output-filename', '-fn', '-ofn', type=str, nargs=1, default="")
     args = parser.parse_args()
     if args.degree_dist:
         graphs = []
@@ -161,7 +174,7 @@ if __name__=="__main__":
                 eigvects = uf.get_data(infile, header_rows=0)
             if "graphparams" in infile:
                 graph_params = uf.get_data(infile, header_rows=0)
-        plot_chunglu_coloring(eigvals, eigvects, graph_params)
+        plot_chunglu_coloring(eigvals, eigvects, graph_params, output_filename=args.output_filename[0])
     elif args.erdosrenyi_embedding:
         eigvals = None
         eigvects = None
@@ -173,7 +186,7 @@ if __name__=="__main__":
                 eigvects, params = uf.get_data(infile, header_rows=1)
             if "graphparams" in infile:
                 graph_params = uf.get_data(infile, header_rows=0)
-        plot_erdosrenyi_embedding(eigvals, eigvects, graph_params)
+        plot_erdosrenyi_embedding(eigvals, eigvects, graph_params, output_filename=args.output_filename[0])
     elif args.erdosrenyi_coloring:
         eigvals = None
         eigvects = None
@@ -185,4 +198,4 @@ if __name__=="__main__":
                 eigvects, params = uf.get_data(infile, header_rows=1)
             if "graphparams" in infile:
                 graph_params = uf.get_data(infile, header_rows=0)
-        plot_erdosrenyi_coloring(eigvals, eigvects, graph_params)
+        plot_erdosrenyi_coloring(eigvals, eigvects, graph_params, output_filename=args.output_filename[0])
